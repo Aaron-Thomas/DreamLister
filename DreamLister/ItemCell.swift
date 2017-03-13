5610b069da38b29f4842cdf795d7f05a83523ec5
Aaron Thomas <aaronthomas2000@hotmail.com>
2017-03-13 21:25:24 +0000

    initial commit

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Aaron Thomas on 24/01/2017.
+//  Copyright © 2017 Aaron Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Aaron Thomas on 23/01/2017.
+//  Copyright © 2017 Aaron Thomas. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
