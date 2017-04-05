60c6d23945fd642289d2f71b69552a0f90b1f40d
Aaron Thomas <aaronthomas2000@hotmail.com>
2017-04-05 21:38:20 +0000

    update project

---
+++ DerivedData/DreamLister/Build/Intermediates/DreamLister.build/Debug-iphoneos/DreamLister.build/DerivedSources/CoreDataGenerated/DreamLister/Store+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Store+CoreDataClass.swift
+//  
+//
+//  Created by Aaron Thomas on 13/03/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
---
+++ DerivedData/DreamLister/Build/Intermediates/DreamLister.build/Debug-iphonesimulator/DreamLister.build/DerivedSources/CoreDataGenerated/DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by Aaron Thomas on 13/03/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
